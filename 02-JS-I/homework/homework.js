5188a1ffec903235ab938827b2b7ab71a45dc128
nahuelsierra1 <nahuel_sierra2@hotmail.com>
2022-03-27 19:34:28 -0300

    primer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -83,6 +83,12 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
   
+  if(str1.length == str2.length){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function menosQueNoventa(num) {
